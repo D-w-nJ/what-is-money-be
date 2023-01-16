0e30507ce0e082c8d2233bd7cdd9ee790858e864
D-w-nJ <jdu2121@ajou.ac.kr>
2023-01-16 16:36:54 +0900

    feat : controller, service, repositoty initialize

--- src/main/java/com/example/demo/src/record/RecordController.java
+++ src/main/java/com/example/demo/src/record/RecordController.java
@@ -1,4 +1,25 @@
 package com.example.demo.src.record;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.record.model.PostRecordReq;
+import com.example.demo.src.record.model.PostRecordRes;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
 public class RecordController {
+
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final RecordService recordService;
+
+    public RecordController(JwtService jwtService, RecordService recordService) {
+        this.jwtService = jwtService;
+        this.recordService = recordService;
+    }
 }
---
+++ src/main/java/com/example/demo/src/record/RecordRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.src.record;
+
+import com.example.demo.src.record.model.RecordEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecordRepository extends JpaRepository<RecordEntity,Long> {
+}
