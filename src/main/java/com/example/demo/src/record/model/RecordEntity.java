4082032afb68e6a503a3cc182da124d3b2304c58
D-w-nJ <jdu2121@ajou.ac.kr>
2023-01-16 16:08:07 +0900

    feat : record entity 생성

--- src/main/java/com/example/demo/src/record/model/RecordEntity.java
+++ src/main/java/com/example/demo/src/record/model/RecordEntity.java
@@ -1,4 +1,31 @@
 package com.example.demo.src.record.model;
 
+import com.example.demo.src.category.model.CategoryEntity;
+import com.example.demo.src.goal.model.GoalEntity;
+import com.example.demo.src.user.model.UserEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity //JPA가 사용하는 객체라는 뜻이다. 이 어노테이션이 있어야 JPA가 인식할 수 있다.
+@Table(name = "record")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Data
 public class RecordEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int amount;
+    private boolean flag; //0:저축, 1: 지출
+
+    @ManyToOne @JoinColumn(name="category_id",referencedColumnName = "id")
+    private CategoryEntity category;
+    @ManyToOne @JoinColumn(name="user_id", referencedColumnName = "id")
+    private UserEntity user;
+    @ManyToOne @JoinColumn(name="goal_id",referencedColumnName = "id")
+    private GoalEntity goal;
 }
