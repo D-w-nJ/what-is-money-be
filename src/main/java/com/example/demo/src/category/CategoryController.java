1dc7a8ee7bcdbe710c1fbc0fb23a9144aee2df10
jooys130 <wndustn130@gmail.com>
2023-01-19 13:17:27 +0900

    feat: 카테고리 조회

--- src/main/java/com/example/demo/src/category/CategoryController.java
+++ src/main/java/com/example/demo/src/category/CategoryController.java
@@ -1,4 +1,45 @@
 package com.example.demo.src.category;
 
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.config.BaseResponseStatus;
+import com.example.demo.src.category.model.GetCategoryRes;
+import com.example.demo.utils.JwtService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("category")
 public class CategoryController {
+
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final CategoryService categoryService;
+
+    public CategoryController(JwtService jwtService, CategoryService categoryService) {
+        this.jwtService = jwtService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("{userIdx}/{flag}")
+    public BaseResponse<List<GetCategoryRes>> getCategoryList(@PathVariable("userIdx") Long userIdx,
+                                                              @PathVariable("flag") int flag) {
+        try {
+            int jwtServiceUserIdx = jwtService.getUserIdx();
+            if (jwtServiceUserIdx != userIdx) {
+                return new BaseResponse<>(BaseResponseStatus.INVALID_USER_JWT);
+            }
+            List<GetCategoryRes> getCategoryResList = categoryService.getCategoryResList(userIdx, flag);
+            return new BaseResponse<>(getCategoryResList);
+        } catch (BaseException exception) {
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
 }
--- src/main/java/com/example/demo/src/category/CategoryRepository.java
+++ src/main/java/com/example/demo/src/category/CategoryRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.src.category;
 
 import com.example.demo.src.category.model.CategoryEntity;
-import com.example.demo.src.record.model.RecordEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.src.category.model.GetCategoryRes;
 
-public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
     CategoryEntity findByCategoryIdx(Long categoryIdx);
+
+    @Query("select new com.example.demo.src.category.model.GetCategoryRes(m.categoryIdx, m.category_name, m.flag) "
+            + "from CategoryEntity m where m.user_id.id = :userIdx and m.flag = :flag")
+    List<GetCategoryRes> findCategoryList(@Param("userIdx") Long userIdx, @Param("flag") int flag);
 }
