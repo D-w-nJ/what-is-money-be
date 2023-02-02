f3efc990191296ae01608b88476f543dcc48c2e6
msung99 <msung6924@naver.com>
2023-02-02 15:10:27 +0900

    refactor: Goal 삭제시 Record 데이터도 함께 삭제

--- src/main/java/com/example/demo/src/record/RecordRepository.java
+++ src/main/java/com/example/demo/src/record/RecordRepository.java
@@ -5,7 +5,9 @@ import com.example.demo.src.record.model.RecordEntity;
 import com.example.demo.src.user.model.UserEntity;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,4 +17,8 @@ public interface RecordRepository extends JpaRepository<RecordEntity, Long> {
 
     @Query(value = "select date from RecordEntity where user=?1 and goal=?2")
     List<LocalDateTime> findDateByUserAndGoal(UserEntity user, GoalEntity goal);
+
+    @Modifying
+    @Query("delete from RecordEntity m where m.goal = :goalEntity")
+    void deleteRecordEntitiesByGoalEntity(GoalEntity goalEntity);
 }
