a066b8cfcd8ee58ab434cf394a2d8ef9c6fa6862
yoonseocho <jessicadbstj@gmail.com>
2023-02-16 10:48:51 +0900

    문의사항API 개발 후 회원탈퇴 안되는 이슈 해결

--- src/main/java/com/example/demo/src/user/UserRepository.java
+++ src/main/java/com/example/demo/src/user/UserRepository.java
@@ -2,12 +2,35 @@ package com.example.demo.src.user;
 
 import com.example.demo.src.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 @EnableJpaRepositories
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    @Modifying
+    @Query("delete from RecordEntity r where r.user.id= :userIdx")
+    void deleteRecord(@Param("userIdx")Long userIdx);
+
+    @Modifying
+    @Query("delete from CategoryEntity c where c.user_id.id= :userIdx")
+    void deleteCategory(@Param("userIdx")Long userIdx);
+
+    @Modifying
+    @Query("delete from GoalEntity g where g.user_id.id= :userIdx")
+    void deleteGoal(@Param("userIdx")Long userIdx);
+
+    @Modifying
+    @Query("delete from QuestionEntity q where q.user.id= :userIdx")
+    void deleteQuestion(@Param("userIdx")Long userIdx);
+//
+    @Modifying
+    @Query("delete from UserEntity u where u.id= :userIdx")
+    void deleteUser(@Param("userIdx")Long userIdx);
+
+
     UserEntity findByEmail(String email);
 
     UserEntity findByUserId(String userId);
